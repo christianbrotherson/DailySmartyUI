d0ae48a771ea52f17c19887c86e8fd0c266d97e1
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-15 16:15:50 -0600

    connected recentPosts to redux and setup thunk

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
+
 
 class RecentPosts extends Component {
+
+    componentDidMount() {
+        this.props.fetchRecentPosts()
+    }
 
     render() {
         return (
@@ -19,4 +27,4 @@ class RecentPosts extends Component {
     }
 }
 
-export default RecentPosts;
+export default connect(null, actions)(RecentPosts);
